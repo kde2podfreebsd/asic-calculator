ff94ccf229cd050b1a923debb1895aad012772f8
jonathch <jonathch@student.21-school.ru>
2024-09-09 00:46:24 +0300

    change count

--- bot/handlers/count.py
+++ bot/handlers/count.py
@@ -1,23 +1,7 @@
 from bot.config import *
 from telebot import types
 
-@bot.callback_query_handler(func=lambda call: call.data.startswith('ths_'), state=CalculatorStates.choose_ths)
-async def choose_count(call):
-    if call.data == 'back':
-        selected_ths = context_manager.current_asic[call.message.chat.id].get('ths')
-    else:
-        selected_ths = call.data.split('_')[1]
-
-    context_manager.fill_current_asic(call.message.chat.id, ths=selected_ths)
-
-    current_context_data = context_manager.current_asic.get(call.message.chat.id, {})
-    message_text = (f'🟢 Алгоритм: {current_context_data.get("algorithm")}\n'
-                    f'🟢 Монета: {current_context_data.get("coin")}\n'
-                    f'🟢 Производитель: {current_context_data.get("manufacturer")}\n'
-                    f'🟢 Модель: {current_context_data.get("model")}\n'
-                    f'🟢 TH/s: {selected_ths}\n'
-                    '...Выберите количество')
-
+def create_number_keyboard():
     markup = types.InlineKeyboardMarkup(row_width=3)
     buttons = [types.InlineKeyboardButton(text=str(i), callback_data=f'num_{i}') for i in range(1, 10)]
     buttons.append(types.InlineKeyboardButton(text='Стереть', callback_data='clear'))
@@ -28,7 +12,36 @@ async def choose_count(call):
     for row in rows:
         markup.row(*row)
     markup.row(types.InlineKeyboardButton(text='Назад', callback_data='back'))
-    await bot.edit_message_text(message_text, call.message.chat.id, msg_ids[call.message.chat.id], reply_markup=markup)
+    return markup
+
+def format_message_text(context_data, number=None):
+    message_text = (f'🟢 Алгоритм: {context_data.get("algorithm")}\n'
+                    f'🟢 Монета: {context_data.get("coin")}\n'
+                    f'🟢 Производитель: {context_data.get("manufacturer")}\n'
+                    f'🟢 Модель: {context_data.get("model")}\n'
+                    f'🟢 TH/s: {context_data.get("ths")}')
+    if number is not None:
+        message_text += f'\n🟢 Количество: {number}'
+    return message_text
+
+async def update_message_text(call, message_text, markup):
+    await bot.edit_message_text(message_text, call.message.chat.id, msg_ids[call.message.chat.id])
+    await bot.edit_message_reply_markup(call.message.chat.id, msg_ids[call.message.chat.id], reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('ths_'), state=CalculatorStates.choose_ths)
+async def choose_count(call):
+    if call.data == 'back':
+        selected_ths = context_manager.current_asic[call.message.chat.id].get('ths')
+    else:
+        selected_ths = call.data.split('_')[1]
+
+    context_manager.fill_current_asic(call.message.chat.id, ths=selected_ths)
+
+    current_context_data = context_manager.current_asic.get(call.message.chat.id, {})
+    message_text = format_message_text(current_context_data)
+
+    markup = create_number_keyboard()
+    await update_message_text(call, message_text, markup)
     await bot.set_state(call.message.chat.id, CalculatorStates.choose_count)
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('num_'), state=CalculatorStates.choose_count)
@@ -40,40 +53,16 @@ async def handle_number(call):
         current_number += number
     context_manager.current_asic[call.message.chat.id]['number'] = current_number
 
-    markup = types.InlineKeyboardMarkup(row_width=3)
-    buttons = [types.InlineKeyboardButton(text=str(i), callback_data=f'num_{i}') for i in range(1, 10)]
-    buttons.append(types.InlineKeyboardButton(text='Стереть', callback_data='clear'))
-    buttons.append(types.InlineKeyboardButton(text='0', callback_data='num_0'))
-    buttons.append(types.InlineKeyboardButton(text='Выбрать', callback_data='submit'))
-
-    rows = [buttons[i:i + 3] for i in range(0, len(buttons), 3)]
-    for row in rows:
-        markup.row(*row)
-    markup.row(types.InlineKeyboardButton(text='Назад', callback_data='back'))
     current_context_data = context_manager.current_asic.get(call.message.chat.id, {})
-    number_display = (f'🟢 Алгоритм: {current_context_data.get("algorithm")}\n'
-                      f'🟢 Монета: {current_context_data.get("coin")}\n'
-                      f'🟢 Производитель: {current_context_data.get("manufacturer")}\n'
-                      f'🟢 Модель: {current_context_data.get("model")}\n'
-                      f'🟢 TH/s: {current_context_data.get("ths")}\n'
-                      f'🟢 Количество: {current_number}')
+    message_text = format_message_text(current_context_data, current_number)
 
-    await bot.edit_message_text(number_display, call.message.chat.id, msg_ids[call.message.chat.id])
-    await bot.edit_message_reply_markup(call.message.chat.id, msg_ids[call.message.chat.id], reply_markup=markup)
+    markup = create_number_keyboard()
+    await update_message_text(call, message_text, markup)
 
 @bot.callback_query_handler(func=lambda call: call.data == 'clear', state=CalculatorStates.choose_count)
 async def handle_clear(call):
     context_manager.current_asic[call.message.chat.id]['number'] = ''
 
-    markup = types.InlineKeyboardMarkup(row_width=3)
-    buttons = [types.InlineKeyboardButton(text=str(i), callback_data=f'num_{i}') for i in range(1, 10)]
-    buttons.append(types.InlineKeyboardButton(text='Стереть', callback_data='clear'))
-    buttons.append(types.InlineKeyboardButton(text='0', callback_data='num_0'))
-    buttons.append(types.InlineKeyboardButton(text='Выбрать', callback_data='submit'))
-
-    rows = [buttons[i:i + 3] for i in range(0, len(buttons), 3)]
-    for row in rows:
-        markup.row(*row)
-    markup.row(types.InlineKeyboardButton(text='Назад', callback_data='back'))
-    await bot.edit_message_text('Кол-во: ', call.message.chat.id, msg_ids[call.message.chat.id])
-    await bot.edit_message_reply_markup(call.message.chat.id, msg_ids[call.message.chat.id], reply_markup=markup)
+    markup = create_number_keyboard()
+    message_text = format_message_text(context_manager.current_asic.get(call.message.chat.id, {}), '')
+    await update_message_text(call, message_text, markup)
